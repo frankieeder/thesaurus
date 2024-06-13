cc7bc82c4d2e405651b4298c0eb0e034934b9a2f
Frankie Eder <frankaeder@gmail.com>
2024-06-13 01:15:43 -0700

    Working ANNou.

---
+++ preprocess_annoy.py
@@ -0,0 +1,25 @@
+from annoy import AnnoyIndex
+from tqdm import tqdm
+import gensim.downloader
+from pathlib import Path
+import pickle
+
+ANNOY_PATH = Path("./annoy/").resolve()
+
+
+if __name__ == "__main__":
+    ANNOY_PATH.mkdir(exist_ok=True)
+
+    model_tqdm = tqdm(list(gensim.downloader.info()['models'].keys())[3:4])
+    for model in model_tqdm:
+        model_tqdm.set_description(f"Processing model {model}")
+        vectors = gensim.downloader.load(model)
+
+        with open(ANNOY_PATH / f'{model}.pkl', 'wb') as f:
+            pickle.dump(vectors.index_to_key, f)
+
+        t = AnnoyIndex(vectors.vectors.shape[1], 'angular')
+        for i, vector in tqdm(enumerate(vectors.vectors)):
+            t.add_item(i, vector)
+        t.build(10)
+        t.save(str(ANNOY_PATH / f"{model}.ann"))
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,35 @@
 import streamlit as st
-import gensim.downloader
+import gensim
+from annoy import AnnoyIndex
+import preprocess_annoy
+import pickle
+
+# st.write([p.stem for p in preprocess_annoy.ANNOY_PATH.iterdir()])
 
 model = st.selectbox(
     label="Model",
-    options=list(gensim.downloader.info()['models'].keys()),
-    index=3,
+    options=[p for p in preprocess_annoy.ANNOY_PATH.iterdir() if p.suffix == '.ann'],
+    format_func=lambda p: p.stem
 )
 
 query = st.text_input(
     label="Query"
 )
 
-vectors = gensim.downloader.load(model)
-st.write(len(vectors.index_to_key))
+with st.spinner("Loading Model..."):
+    info = gensim.downloader.info()['models'][model.stem]
+    u = AnnoyIndex(info['parameters']['dimension'], 'angular')
+    u.load(str(model))
+
+    with open(preprocess_annoy.ANNOY_PATH / f"{model.stem}.pkl", 'rb') as f:
+        index_to_key = pickle.load(f)
+        key_to_index = {v:i for i, v in enumerate(index_to_key)}
 
 if query:
-    st.write(vectors.most_similar(query))
-
-
+    if query not in key_to_index:
+        st.error("Word not found... try another")
+    else:
+        index = key_to_index[query]
+        nns = u.get_nns_by_item(index, 100, include_distances=True)
+        for ranking, (nn, score) in enumerate(zip(*nns)):
+            st.write(f"#{ranking} | {score}: {index_to_key[nn]}")

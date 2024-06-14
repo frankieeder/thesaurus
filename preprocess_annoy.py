293c935af1e5f0474a68daa9b14c489e0bd916de
Frankie Eder <frankaeder@gmail.com>
2024-06-13 17:48:04 -0700

    Reduce # vectors.

--- preprocess_annoy.py
+++ preprocess_annoy.py
@@ -14,8 +14,10 @@ def download_model(model):
     with open(ANNOY_PATH / f'{model}.pkl', 'wb') as f:
         pickle.dump(vectors.index_to_key, f)
 
+    reduction_rate = 1 / 20
+
     t = AnnoyIndex(vectors.vectors.shape[1], 'angular')
-    for i, vector in tqdm(enumerate(vectors.vectors)):
+    for i, vector in tqdm(enumerate(vectors.vectors[:int(vectors.vectors.shape[0] * reduction_rate), :])):
         t.add_item(i, vector)
     t.build(10)
     t.save(str(ANNOY_PATH / f"{model}.ann"))
@@ -26,7 +28,7 @@ if __name__ == "__main__":
     models = list(gensim.downloader.info()['models'].keys())
     print(models)
 
-    model_tqdm = tqdm(models[2:3])
+    model_tqdm = tqdm(models[:1])
     for model in model_tqdm:
         model_tqdm.set_description(f"Processing model {model}")
         download_model(model)
